f2a6597e26ed2a997639047e38bcca69adeb5997
edinardo <edinardobezerra@yahoo.com.br>
2022-06-25 11:52:29 -0300

    arquivo de introdução ao javascript

---
+++ primeiro_programa.js
@@ -0,0 +1,34 @@
+console.log("ola turma do itunung")
+console.log("tudo bem com vcs")
+let nome = "jô, instrutora"
+console.log(nome)
+let bananinha = 12
+console.log(bananinha)
+console.log("Edinardo")
+let x = false
+let y = true
+
+let lista = ["teste", "nome", "idade", 4, "Tiago, Jô", false, true,y]
+console.log(lista)
+
+let objetovoador = {
+    "nome": "edinardo",
+    "idade": 45,
+    "casado": true,
+    "livro_favorito": ["neuromancer"]
+}
+console.log(objetovoador)
+
+let variavel_1 = "1"
+
+let variavel_2 = 10
+console.log(variavel_1 + variavel_2)
+console.log(10)
+console.log(20)
+
+let idade = 18
+if (idade >= 18) {
+    console.log("pode jogar")
+} else {
+    console.log("nao pode jogar")
+}
